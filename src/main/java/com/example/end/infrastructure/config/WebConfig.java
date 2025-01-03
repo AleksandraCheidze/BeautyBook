afea08d65179d2c81b69b383b806b035a5f7c36b
Dmitrijs Loginovs <132077317+Loginofff@users.noreply.github.com>
2025-01-03 17:15:44 +0100

    cors config

--- src/main/java/com/example/end/infrastructure/config/WebConfig.java
+++ src/main/java/com/example/end/infrastructure/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.example.end.infrastructure.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,9 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+        registry.addMapping("/**") // Разрешить любые пути
+                .allowedOrigins("*") // Разрешить запросы с любых доменов
+                .allowedMethods("*") // Разрешить любые HTTP методы
+                .allowedHeaders("*"); // Разрешить любые заголовки
     }
 }
