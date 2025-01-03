f28a35765dbf50f3fad6f7dd4d80efad5536b6e0
Aleksandra Cheidze <a.cxeidze@gmail.com>
2025-01-03 17:18:22 +0100

    add webConfig

--- src/main/java/com/example/end/infrastructure/config/WebConfig.java
+++ src/main/java/com/example/end/infrastructure/config/WebConfig.java
@@ -10,9 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
                 .allowedHeaders("*");
     }
 }
