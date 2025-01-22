f3d295d34fa5ede70b08350777b9fd8faa764495
Aleksandra Cheidze <a.cxeidze@gmail.com>
2025-01-22 19:19:40 +0100

    fix uploadPortfolioPhotos

--- src/main/java/com/example/end/dto/PortfolioImageDto.java
+++ src/main/java/com/example/end/dto/PortfolioImageDto.java
@@ -1,15 +1,7 @@
 package com.example.end.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.*;
 
-import java.util.List;
-import java.util.Set;
 
 @Getter
 @Setter
@@ -18,10 +10,7 @@ import java.util.Set;
 @NoArgsConstructor
 public class PortfolioImageDto {
 
-    @NotNull
-    @Size(min = 1)
-    @Schema(description = "Portfolio image URLs of the user", example = "[\"https://example.com/image1.jpg\",\"https://example.com/image2.jpg\"]")
-    private List<String> portfolioImageUrls;
+
 
     private Long id;
 
