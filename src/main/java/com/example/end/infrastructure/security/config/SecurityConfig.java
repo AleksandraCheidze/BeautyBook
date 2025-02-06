0806c53e762d589d8cb7abeb0d5ec267ade2f7ac
Aleksandra Cheidze <a.cxeidze@gmail.com>
2025-02-06 15:57:34 +0100

    update SecurityConfig

--- src/main/java/com/example/end/infrastructure/security/config/SecurityConfig.java
+++ src/main/java/com/example/end/infrastructure/security/config/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedOriginPatterns(List.of("*"));  // Разрешает любые источники
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Authorization", "Content-Type"));
@@ -81,6 +81,7 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
 
     @Bean
     public OpenAPI openAPI() {
