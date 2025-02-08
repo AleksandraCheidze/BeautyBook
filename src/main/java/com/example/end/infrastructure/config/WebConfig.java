83bf737519288d15b46922906fd4e7b658ecc38a
Aleksandra Cheidze <a.cxeidze@gmail.com>
2025-02-08 13:40:56 +0100

    update config

--- src/main/java/com/example/end/infrastructure/config/WebConfig.java
+++ src/main/java/com/example/end/infrastructure/config/WebConfig.java
@@ -1,17 +1,17 @@
-//package com.example.end.infrastructure.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:3000", "https://beauty-book-3-0.vercel.app")
-//                .allowedMethods("*")
-//                .allowedHeaders("*");
-//    }
-//}
+package com.example.end.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "https://beauty-book-3-0.vercel.app")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
