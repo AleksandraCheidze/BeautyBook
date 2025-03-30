86c16371eb91075f7420c15a71b41ca48b7bc4ab
Dmitry rogov <a.cxeidze@gmail.com>
2025-03-30 22:36:08 +0200

    Simplify Docker build process, remove separate dependency download step

--- src/main/java/com/example/end/config/RedisConfig.java
+++ src/main/java/com/example/end/config/RedisConfig.java
@@ -1,12 +1,12 @@
 package com.example.end.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.annotation.EnableCaching;
+// import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -14,14 +14,14 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import java.net.URI;
 
 @Configuration
-@EnableCaching
+// @EnableCaching
 public class RedisConfig {
 
     @Value("${spring.data.redis.url}")
     private String redisUrl;
 
     @Bean
-    public JedisConnectionFactory jedisConnectionFactory() {
+    public LettuceConnectionFactory redisConnectionFactory() {
         URI uri = URI.create(redisUrl);
         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(uri.getHost(), uri.getPort());
         if (uri.getUserInfo() != null) {
@@ -29,8 +29,11 @@ public class RedisConfig {
             if (userInfo.length > 1) {
                 config.setPassword(userInfo[1]);
             }
+            if (userInfo.length > 0) {
+                config.setUsername(userInfo[0]);
+            }
         }
-        return new JedisConnectionFactory(config);
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
@@ -41,6 +44,7 @@ public class RedisConfig {
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
         template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setEnableTransactionSupport(true);
         return template;
     }
 }
